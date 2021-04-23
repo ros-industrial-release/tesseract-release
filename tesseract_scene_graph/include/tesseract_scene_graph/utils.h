65e525f9469f7e3f4c08f8471cec8f00435a9c38
Levi Armstrong <levi.armstrong@swri.org>
2021-04-23 16:24:36 -0500

    Rebase from 'upstream'

--- tesseract_scene_graph/include/tesseract_scene_graph/utils.h
+++ tesseract_scene_graph/include/tesseract_scene_graph/utils.h
@@ -1,19 +1,41 @@
+/**
+ * @file utils.h
+ * @brief Tesseract Scene Graph utility functions
+ *
+ * @author Levi Armstrong
+ * @date Dec 18, 2017
+ * @version TODO
+ * @bug No known bugs
+ *
+ * @copyright Copyright (c) 2017, Southwest Research Institute
+ *
+ * @par License
+ * Software License Agreement (Apache License)
+ * @par
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * @par
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 #ifndef TESSERACT_SCENE_GRAPH_UTILS_H
 #define TESSERACT_SCENE_GRAPH_UTILS_H
 
+#include <tesseract_common/macros.h>
+TESSERACT_COMMON_IGNORE_WARNINGS_PUSH
+#include <vector>
+#include <string>
+TESSERACT_COMMON_IGNORE_WARNINGS_POP
+
 #include <tesseract_scene_graph/allowed_collision_matrix.h>
-#include <tesseract_scene_graph/graph.h>
-#include <tesseract_scene_graph/srdf_model.h>
-#include <functional>
 
 namespace tesseract_scene_graph
 {
-inline void processSRDFAllowedCollisions(SceneGraph& scene_graph, const tesseract_scene_graph::SRDFModel& srdf_model)
-{
-  for (const auto& pair : srdf_model.acm.getAllAllowedCollisions())
-    scene_graph.addAllowedCollision(pair.first.first, pair.first.second, pair.second);
-}
-
 /**
  * @brief Gets allowed collisions for a set of link names.
  * @param link_names Vector of link names for which we want the allowed collisions
@@ -21,30 +43,9 @@ inline void processSRDFAllowedCollisions(SceneGraph& scene_graph, const tesserac
  * @param remove_duplicates If true, duplicates will be removed. Default: true
  * @return vector of links that are allowed to collide with links given
  */
-inline std::vector<std::string> getAllowedCollisions(const std::vector<std::string>& link_names,
-                                                     const AllowedCollisionEntries& acm_entries,
-                                                     bool remove_duplicates = true)
-{
-  std::vector<std::string> results;
-  results.reserve(acm_entries.size());
-
-  for (const auto& entry : acm_entries)
-  {
-    const std::string link_1 = entry.first.first;
-    const std::string link_2 = entry.first.second;
-
-    // If the first entry is one of the links we were looking for
-    if (std::find(link_names.begin(), link_names.end(), link_1) != link_names.end())
-      // If it hasn't already been added or remove_duplicates is disabled
-      if (!remove_duplicates || (std::find(results.begin(), results.end(), link_2) == results.end()))
-        results.push_back(link_2);
-
-    if (std::find(link_names.begin(), link_names.end(), link_2) != link_names.end())
-      if (!remove_duplicates || (std::find(results.begin(), results.end(), link_1) == results.end()))
-        results.push_back(link_1);
-  }
-  return results;
-}
+std::vector<std::string> getAllowedCollisions(const std::vector<std::string>& link_names,
+                                              const AllowedCollisionEntries& acm_entries,
+                                              bool remove_duplicates = true);
 
 }  // namespace tesseract_scene_graph
 
