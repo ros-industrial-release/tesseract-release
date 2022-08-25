cef4062d710048271e36502b7b8dd1f4a69e8a7b
Levi Armstrong <levi.armstrong@gmail.com>
2022-08-25 11:05:15 -0500

    Revert "Trimmed the branch to only the tesseract_kinematics sub directory"
    This reverts commit 0ad409a4265530ee9b508b8e56cd9f06d2e931f3.

---
+++ tesseract_common/cmake/tesseract_macros.cmake
@@ -0,0 +1,194 @@
+#
+# @file tesseract_macros.cmae @brief Common Tesseract CMake Macros
+#
+# @author Levi Armstrong @date October 15, 2019 @version TODO @bug No known bugs
+#
+# @copyright Copyright (c) 2019, Southwest Research Institute
+#
+# @par License Software License Agreement (Apache License) @par Licensed under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance with the License. You may obtain a copy of the License at
+# http://www.apache.org/licenses/LICENSE-2.0 @par Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+# express or implied. See the License for the specific language governing permissions and limitations under the License.
+
+macro(tesseract_variables)
+  if(NOT DEFINED BUILD_SHARED_LIBS)
+    set(BUILD_SHARED_LIBS ON)
+  endif()
+
+  if(NOT DEFINED TESSERACT_ENABLE_EXAMPLES)
+    set(TESSERACT_ENABLE_EXAMPLES ON)
+  endif()
+
+  if(NOT DEFINED TESSERACT_ENABLE_CLANG_TIDY)
+    set(TESSERACT_ENABLE_CLANG_TIDY OFF)
+  endif()
+
+  if(NOT DEFINED TESSERACT_ENABLE_CODE_COVERAGE)
+    set(TESSERACT_ENABLE_CODE_COVERAGE OFF)
+  elseif(TESSERACT_ENABLE_CODE_COVERAGE)
+    set(TESSERACT_ENABLE_TESTING ON)
+  endif()
+
+  if(NOT DEFINED TESSERACT_ENABLE_TESTING)
+    set(TESSERACT_ENABLE_TESTING OFF)
+  endif()
+
+  if(NOT DEFINED TESSERACT_ENABLE_EXAMPLES)
+    set(TESSERACT_ENABLE_EXAMPLES ON)
+  endif()
+
+  if(NOT DEFINED TESSERACT_WARNINGS_AS_ERRORS)
+    set(TESSERACT_WARNINGS_AS_ERRORS ${TESSERACT_ENABLE_TESTING})
+  endif()
+
+  if(NOT DEFINED TESSERACT_ENABLE_RUN_TESTING)
+    set(TESSERACT_ENABLE_RUN_TESTING OFF)
+  endif()
+
+  if(TESSERACT_ENABLE_TESTING_ALL)
+    set(TESSERACT_ENABLE_TESTING ON)
+    set(TESSERACT_ENABLE_CLANG_TIDY ON)
+    set(TESSERACT_ENABLE_CODE_COVERAGE ON)
+  endif()
+
+  set(TESSERACT_COMPILE_DEFINITIONS "")
+  set(TESSERACT_COMPILE_OPTIONS_PUBLIC "")
+  set(TESSERACT_COMPILE_OPTIONS_PRIVATE "")
+  if(NOT TESSERACT_WARNINGS_AS_ERRORS)
+    if(CMAKE_CXX_COMPILER_ID STREQUAL "GNU")
+      set(TESSERACT_COMPILE_OPTIONS_PRIVATE
+          -Wall
+          -Wextra
+          -Wconversion
+          -Wsign-conversion
+          -Wno-sign-compare
+          -Wnon-virtual-dtor)
+      exec_program(uname ARGS -p OUTPUT_VARIABLE CMAKE_SYSTEM_NAME2)
+      if(NOT
+         CMAKE_SYSTEM_NAME2
+         MATCHES
+         "aarch64"
+         AND NOT
+             CMAKE_SYSTEM_NAME2
+             MATCHES
+             "armv7l"
+         AND NOT
+             CMAKE_SYSTEM_NAME2
+             MATCHES
+             "unknown")
+        set(TESSERACT_COMPILE_OPTIONS_PUBLIC -mno-avx)
+      endif()
+    elseif(CMAKE_CXX_COMPILER_ID STREQUAL "Clang")
+      set(TESSERACT_COMPILE_OPTIONS_PRIVATE
+          -Wall
+          -Wextra
+          -Wconversion
+          -Wsign-conversion)
+      message(WARNING "Non-GNU compiler detected. If using AVX instructions, Eigen alignment issues may result.")
+    elseif(CMAKE_CXX_COMPILER_ID STREQUAL "MSVC")
+      set(TESSERACT_COMPILE_DEFINITIONS "_USE_MATH_DEFINES=ON")
+      message(WARNING "Non-GNU compiler detected. If using AVX instructions, Eigen alignment issues may result.")
+    else()
+      message(WARNING "${CMAKE_CXX_COMPILER_ID} Unsupported compiler detected.")
+    endif()
+  else()
+    if(CMAKE_CXX_COMPILER_ID STREQUAL "GNU")
+      set(TESSERACT_COMPILE_OPTIONS_PRIVATE
+          -Werror=all
+          -Werror=extra
+          -Werror=conversion
+          -Werror=sign-conversion
+          -Wno-sign-compare
+          -Werror=non-virtual-dtor)
+      exec_program(uname ARGS -p OUTPUT_VARIABLE CMAKE_SYSTEM_NAME2)
+      if(NOT
+         CMAKE_SYSTEM_NAME2
+         MATCHES
+         "aarch64"
+         AND NOT
+             CMAKE_SYSTEM_NAME2
+             MATCHES
+             "armv7l"
+         AND NOT
+             CMAKE_SYSTEM_NAME2
+             MATCHES
+             "unknown")
+        set(TESSERACT_COMPILE_OPTIONS_PUBLIC -mno-avx)
+      endif()
+    elseif(CMAKE_CXX_COMPILER_ID STREQUAL "Clang")
+      set(TESSERACT_COMPILE_OPTIONS_PRIVATE
+          -Werror=all
+          -Werror=extra
+          -Werror=conversion
+          -Werror=sign-conversion)
+      message(WARNING "Non-GNU compiler detected. If using AVX instructions, Eigen alignment issues may result.")
+    elseif(CMAKE_CXX_COMPILER_ID STREQUAL "MSVC")
+      set(TESSERACT_COMPILE_DEFINITIONS "_USE_MATH_DEFINES=ON")
+      message(WARNING "Non-GNU compiler detected. If using AVX instructions, Eigen alignment issues may result.")
+    else()
+      message(WARNING "${CMAKE_CXX_COMPILER_ID} Unsupported compiler detected.")
+    endif()
+  endif()
+
+  set(TESSERACT_CXX_VERSION 17)
+endmacro()
+
+macro(tesseract_cpack)
+  set(oneValueArgs
+      VERSION
+      MAINTAINER
+      DESCRIPTION
+      LICENSE_FILE
+      README_FILE)
+  set(multiValueArgs LINUX_DEPENDS WINDOWS_DEPENDS)
+  cmake_parse_arguments(
+    ARG
+    ""
+    "${oneValueArgs}"
+    "${multiValueArgs}"
+    ${ARGN})
+
+  set(CPACK_PACKAGE_VENDOR "ROS-Industrial")
+  set(CPACK_RESOURCE_FILE_LICENSE ${ARG_LICENSE_FILE})
+  set(CPACK_RESOURCE_FILE_README ${ARG_README_FILE})
+  if(UNIX)
+    string(
+      REPLACE "_"
+              "-"
+              TESSERACT_PACKAGE_NAME
+              ${PROJECT_NAME})
+    set(CPACK_GENERATOR "DEB;TXZ")
+
+    if(CMAKE_SYSTEM_PROCESSOR STREQUAL "x86_64")
+      set(DEB_ARCH "amd64")
+    else()
+      set(DEB_ARCH ${CMAKE_SYSTEM_PROCESSOR})
+    endif()
+
+    set(CPACK_PACKAGE_FILE_NAME "${TESSERACT_PACKAGE_PREFIX}${TESSERACT_PACKAGE_NAME}_${DEB_ARCH}_linux_${ARG_VERSION}")
+    set(CPACK_DEBIAN_PACKAGE_NAME "${TESSERACT_PACKAGE_PREFIX}${TESSERACT_PACKAGE_NAME}")
+    set(CPACK_DEBIAN_PACKAGE_ARCHITECTURE ${DEB_ARCH})
+    set(CPACK_DEBIAN_PACKAGE_MAINTAINER ${ARG_MAINTAINER})
+    set(CPACK_DEBIAN_PACKAGE_DESCRIPTION ${ARG_DESCRIPTION})
+    set(CPACK_DEBIAN_PACKAGE_SHLIBDEPS=ON)
+    string(
+      REPLACE ";"
+              ","
+              CPACK_DEBIAN_PACKAGE_DEPENDS
+              "${ARG_LINUX_DEPENDS}")
+  elseif(WIN32)
+    set(CPACK_GENERATOR "NuGet;TXZ")
+    set(CPACK_PACKAGE_FILE_NAME
+        "${TESSERACT_PACKAGE_PREFIX}${TESSERACT_PACKAGE_NAME}_${CMAKE_SYSTEM_PROCESSOR}_windows_${ARG_VERSION}")
+    set(CPACK_NUGET_PACKAGE_NAME
+        "${TESSERACT_PACKAGE_PREFIX}${TESSERACT_PACKAGE_NAME}_${CMAKE_SYSTEM_PROCESSOR}_windows")
+    set(CPACK_NUGET_PACKAGE_DESCRIPTION ${ARG_DESCRIPTION})
+    string(
+      REPLACE ";"
+              ","
+              CPACK_NUGET_PACKAGE_DEPENDENCIES
+              "${ARG_WINDOWS_DEPENDS}")
+  endif()
+  include(CPack)
+endmacro()
