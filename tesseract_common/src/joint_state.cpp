b17542c40729c526e19bfb7dca6879348eb6cb99
Levi Armstrong <levi.armstrong@gmail.com>
2023-03-03 10:06:13 -0600

    Revert "Trimmed the branch to only the tesseract_state_solver sub directory"
    This reverts commit b4e7fe9bcc985e176a0094fc874d9bd13bd8e6fa.

---
+++ tesseract_common/src/joint_state.cpp
@@ -0,0 +1,178 @@
+/**
+ * @file joint_state.cpp
+ * @brief Tesseract Joint State
+ *
+ * @author Levi Armstrong
+ * @date January 18, 2018
+ * @version TODO
+ * @bug No known bugs
+ *
+ * @copyright Copyright (c) 2017, Southwest Research Institute
+ *
+ * @par License
+ * Software License Agreement (Apache License)
+ * @par
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * @par
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+#include <tesseract_common/macros.h>
+TESSERACT_COMMON_IGNORE_WARNINGS_PUSH
+#include <boost/serialization/nvp.hpp>
+#include <boost/serialization/string.hpp>
+#include <boost/serialization/vector.hpp>
+TESSERACT_COMMON_IGNORE_WARNINGS_POP
+
+#include <tesseract_common/utils.h>
+#include <tesseract_common/eigen_serialization.h>
+#include <tesseract_common/joint_state.h>
+
+namespace tesseract_common
+{
+JointState::JointState(std::vector<std::string> joint_names, Eigen::VectorXd position)
+  : joint_names(std::move(joint_names)), position(std::move(position))
+{
+}
+
+bool JointState::operator==(const JointState& other) const
+{
+  bool ret_val = true;
+  ret_val &= (joint_names == other.joint_names);
+  ret_val &= (position.isApprox(other.position, 1e-5));
+  ret_val &= (velocity.isApprox(other.velocity, 1e-5));
+  ret_val &= (acceleration.isApprox(other.acceleration, 1e-5));
+  ret_val &= (effort.isApprox(other.effort, 1e-5));
+  ret_val &= (tesseract_common::almostEqualRelativeAndAbs(time, other.time, 1e-5));
+  return ret_val;
+}
+
+bool JointState::operator!=(const JointState& rhs) const { return !operator==(rhs); }
+
+template <class Archive>
+void JointState::serialize(Archive& ar, const unsigned int /*version*/)  // NOLINT
+{
+  ar& BOOST_SERIALIZATION_NVP(joint_names);
+  ar& BOOST_SERIALIZATION_NVP(position);
+  ar& BOOST_SERIALIZATION_NVP(velocity);
+  ar& BOOST_SERIALIZATION_NVP(acceleration);
+  ar& BOOST_SERIALIZATION_NVP(effort);
+  ar& BOOST_SERIALIZATION_NVP(time);
+}
+
+JointTrajectory::JointTrajectory(std::string description) : description(std::move(description)) {}
+
+JointTrajectory::JointTrajectory(std::vector<JointState> states, std::string description)
+  : states(std::move(states)), description(std::move(description))
+{
+}
+
+bool JointTrajectory::operator==(const JointTrajectory& other) const
+{
+  bool ret_val = true;
+  ret_val &= (description == other.description);
+  ret_val &= (states == other.states);
+  return ret_val;
+}
+
+bool JointTrajectory::operator!=(const JointTrajectory& rhs) const { return !operator==(rhs); }
+
+///////////////
+// Iterators //
+///////////////
+JointTrajectory::iterator JointTrajectory::begin() { return states.begin(); }
+JointTrajectory::const_iterator JointTrajectory::begin() const { return states.begin(); }
+JointTrajectory::iterator JointTrajectory::end() { return states.end(); }
+JointTrajectory::const_iterator JointTrajectory::end() const { return states.end(); }
+JointTrajectory::reverse_iterator JointTrajectory::rbegin() { return states.rbegin(); }
+JointTrajectory::const_reverse_iterator JointTrajectory::rbegin() const { return states.rbegin(); }
+JointTrajectory::reverse_iterator JointTrajectory::rend() { return states.rend(); }
+JointTrajectory::const_reverse_iterator JointTrajectory::rend() const { return states.rend(); }
+JointTrajectory::const_iterator JointTrajectory::cbegin() const { return states.cbegin(); }
+JointTrajectory::const_iterator JointTrajectory::cend() const { return states.cend(); }
+JointTrajectory::const_reverse_iterator JointTrajectory::crbegin() const { return states.crbegin(); }
+JointTrajectory::const_reverse_iterator JointTrajectory::crend() const { return states.crend(); }
+
+//////////////
+// Capacity //
+//////////////
+bool JointTrajectory::empty() const { return states.empty(); }
+JointTrajectory::size_type JointTrajectory::size() const { return states.size(); }
+JointTrajectory::size_type JointTrajectory::max_size() const { return states.max_size(); }
+void JointTrajectory::reserve(size_type n) { states.reserve(n); }
+JointTrajectory::size_type JointTrajectory::capacity() const { return states.capacity(); }
+void JointTrajectory::shrink_to_fit() { states.shrink_to_fit(); }
+
+////////////////////
+// Element Access //
+////////////////////
+JointTrajectory::reference JointTrajectory::front() { return states.front(); }
+JointTrajectory::const_reference JointTrajectory::front() const { return states.front(); }
+JointTrajectory::reference JointTrajectory::back() { return states.back(); }
+JointTrajectory::const_reference JointTrajectory::back() const { return states.back(); }
+JointTrajectory::reference JointTrajectory::at(size_type n) { return states.at(n); }
+JointTrajectory::const_reference JointTrajectory::at(size_type n) const { return states.at(n); }
+JointTrajectory::pointer JointTrajectory::data() { return states.data(); }
+JointTrajectory::const_pointer JointTrajectory::data() const { return states.data(); }
+JointTrajectory::reference JointTrajectory::operator[](size_type pos) { return states[pos]; }
+JointTrajectory::const_reference JointTrajectory::operator[](size_type pos) const { return states[pos]; };
+
+///////////////
+// Modifiers //
+///////////////
+void JointTrajectory::clear() { states.clear(); }
+JointTrajectory::iterator JointTrajectory::insert(const_iterator p, const value_type& x) { return states.insert(p, x); }
+JointTrajectory::iterator JointTrajectory::insert(const_iterator p, value_type&& x) { return states.insert(p, x); }
+JointTrajectory::iterator JointTrajectory::insert(const_iterator p, std::initializer_list<value_type> l)
+{
+  return states.insert(p, l);
+}
+
+template <class... Args>
+JointTrajectory::iterator JointTrajectory::emplace(const_iterator pos, Args&&... args)
+{
+  return states.emplace(pos, std::forward<Args>(args)...);
+}
+
+JointTrajectory::iterator JointTrajectory::erase(const_iterator p) { return states.erase(p); }
+JointTrajectory::iterator JointTrajectory::erase(const_iterator first, const_iterator last)
+{
+  return states.erase(first, last);
+}
+void JointTrajectory::push_back(const value_type& x) { states.push_back(x); }
+void JointTrajectory::push_back(const value_type&& x) { states.push_back(x); }
+
+template <typename... Args>
+#if __cplusplus > 201402L
+JointTrajectory::reference JointTrajectory::emplace_back(Args&&... args)
+{
+  return states.emplace_back(std::forward<Args>(args)...);
+}
+#else
+void JointTrajectory::emplace_back(Args&&... args)
+{
+  container_.emplace_back(std::forward<Args>(args)...);
+}
+#endif
+
+void JointTrajectory::pop_back() { states.pop_back(); }
+void JointTrajectory::swap(std::vector<value_type>& other) { states.swap(other); }
+
+template <class Archive>
+void JointTrajectory::serialize(Archive& ar, const unsigned int version)  // NOLINT
+{
+  ar& BOOST_SERIALIZATION_NVP(states);
+  ar& BOOST_SERIALIZATION_NVP(description);
+}
+
+}  // namespace tesseract_common
+
+#include <tesseract_common/serialization.h>
+TESSERACT_SERIALIZE_ARCHIVES_INSTANTIATE(tesseract_common::JointState)
+TESSERACT_SERIALIZE_ARCHIVES_INSTANTIATE(tesseract_common::JointTrajectory)
---
+++ tesseract_common/src/types.cpp
@@ -0,0 +1,269 @@
+/**
+ * @file types.cpp
+ * @brief Common Tesseract Types
+ *
+ * @author Levi Armstrong
+ * @date January 18, 2018
+ * @version TODO
+ * @bug No known bugs
+ *
+ * @copyright Copyright (c) 2017, Southwest Research Institute
+ *
+ * @par License
+ * Software License Agreement (Apache License)
+ * @par
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * @par
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <tesseract_common/macros.h>
+TESSERACT_COMMON_IGNORE_WARNINGS_PUSH
+#include <boost/serialization/access.hpp>
+#include <boost/serialization/base_object.hpp>
+#include <boost/serialization/map.hpp>
+#include <boost/serialization/nvp.hpp>
+#include <boost/serialization/set.hpp>
+#include <boost/serialization/split_member.hpp>
+#include <boost/serialization/vector.hpp>
+#include <boost/serialization/unordered_map.hpp>
+#include <memory>
+TESSERACT_COMMON_IGNORE_WARNINGS_POP
+
+#include <tesseract_common/eigen_serialization.h>
+#include <tesseract_common/utils.h>
+#include <tesseract_common/types.h>
+#include <tesseract_common/yaml_utils.h>
+
+namespace tesseract_common
+{
+std::size_t PairHash::operator()(const LinkNamesPair& pair) const
+{
+  return std::hash<std::string>()(pair.first + pair.second);
+}
+
+LinkNamesPair makeOrderedLinkPair(const std::string& link_name1, const std::string& link_name2)
+{
+  if (link_name1 <= link_name2)
+    return std::make_pair(link_name1, link_name2);
+
+  return std::make_pair(link_name2, link_name1);
+}
+
+/*********************************************************/
+/******               PluginInfo                     *****/
+/*********************************************************/
+
+std::string PluginInfo::getConfigString() const
+{
+  std::stringstream stream;
+  stream << config;
+  return stream.str();
+}
+
+bool PluginInfo::operator==(const PluginInfo& rhs) const
+{
+  bool equal = true;
+  equal &= class_name == rhs.class_name;
+  equal &= isIdentical(config, rhs.config);
+
+  return equal;
+}
+bool PluginInfo::operator!=(const PluginInfo& rhs) const { return !operator==(rhs); }
+
+template <class Archive>
+void PluginInfo::save(Archive& ar, const unsigned int /*version*/) const
+{
+  ar& BOOST_SERIALIZATION_NVP(class_name);
+  std::string config_string = getConfigString();
+  ar& BOOST_SERIALIZATION_NVP(config_string);
+}
+
+template <class Archive>
+void PluginInfo::load(Archive& ar, const unsigned int /*version*/)
+{
+  ar& BOOST_SERIALIZATION_NVP(class_name);
+  std::string config_string;
+  ar& BOOST_SERIALIZATION_NVP(config_string);
+  config = YAML::Load(config_string);
+}
+
+template <class Archive>
+void PluginInfo::serialize(Archive& ar, const unsigned int version)
+{
+  boost::serialization::split_member(ar, *this, version);
+}
+
+/*********************************************************/
+/******           PluginInfoContainer                *****/
+/*********************************************************/
+
+void PluginInfoContainer::clear()
+{
+  default_plugin.clear();
+  plugins.clear();
+}
+
+bool PluginInfoContainer::operator==(const PluginInfoContainer& rhs) const
+{
+  bool equal = true;
+  equal &= default_plugin == rhs.default_plugin;
+  equal &= isIdenticalMap<PluginInfoMap, PluginInfo>(plugins, rhs.plugins);
+
+  return equal;
+}
+bool PluginInfoContainer::operator!=(const PluginInfoContainer& rhs) const { return !operator==(rhs); }
+
+template <class Archive>
+void PluginInfoContainer::serialize(Archive& ar, const unsigned int /*version*/)
+{
+  ar& BOOST_SERIALIZATION_NVP(default_plugin);
+  ar& BOOST_SERIALIZATION_NVP(plugins);
+}
+
+/*********************************************************/
+/******           KinematicsPluginInfo               *****/
+/*********************************************************/
+void KinematicsPluginInfo::insert(const KinematicsPluginInfo& other)
+{
+  search_paths.insert(other.search_paths.begin(), other.search_paths.end());
+  search_libraries.insert(other.search_libraries.begin(), other.search_libraries.end());
+  fwd_plugin_infos.insert(other.fwd_plugin_infos.begin(), other.fwd_plugin_infos.end());
+  inv_plugin_infos.insert(other.inv_plugin_infos.begin(), other.inv_plugin_infos.end());
+}
+
+void KinematicsPluginInfo::clear()
+{
+  search_paths.clear();
+  search_libraries.clear();
+  fwd_plugin_infos.clear();
+  inv_plugin_infos.clear();
+}
+
+bool KinematicsPluginInfo::empty() const
+{
+  return (search_paths.empty() && search_libraries.empty() && fwd_plugin_infos.empty() && inv_plugin_infos.empty());
+}
+
+bool KinematicsPluginInfo::operator==(const KinematicsPluginInfo& rhs) const
+{
+  bool equal = true;
+  equal &= isIdenticalSet<std::string>(search_paths, rhs.search_paths);
+  equal &= isIdenticalSet<std::string>(search_libraries, rhs.search_libraries);
+  equal &= fwd_plugin_infos == rhs.fwd_plugin_infos;
+  equal &= inv_plugin_infos == rhs.inv_plugin_infos;
+
+  return equal;
+}
+bool KinematicsPluginInfo::operator!=(const KinematicsPluginInfo& rhs) const { return !operator==(rhs); }
+
+template <class Archive>
+void KinematicsPluginInfo::serialize(Archive& ar, const unsigned int /*version*/)
+{
+  ar& BOOST_SERIALIZATION_NVP(search_paths);
+  ar& BOOST_SERIALIZATION_NVP(search_libraries);
+  ar& BOOST_SERIALIZATION_NVP(fwd_plugin_infos);
+  ar& BOOST_SERIALIZATION_NVP(inv_plugin_infos);
+}
+
+/*********************************************************/
+/******          ContactManagersPluginInfo           *****/
+/*********************************************************/
+void ContactManagersPluginInfo::insert(const ContactManagersPluginInfo& other)
+{
+  search_paths.insert(other.search_paths.begin(), other.search_paths.end());
+  search_libraries.insert(other.search_libraries.begin(), other.search_libraries.end());
+  discrete_plugin_infos.plugins.insert(other.discrete_plugin_infos.plugins.begin(),
+                                       other.discrete_plugin_infos.plugins.end());
+  continuous_plugin_infos.plugins.insert(other.continuous_plugin_infos.plugins.begin(),
+                                         other.continuous_plugin_infos.plugins.end());
+
+  if (!other.discrete_plugin_infos.default_plugin.empty())
+    discrete_plugin_infos.default_plugin = other.discrete_plugin_infos.default_plugin;
+
+  if (!other.continuous_plugin_infos.default_plugin.empty())
+    continuous_plugin_infos.default_plugin = other.continuous_plugin_infos.default_plugin;
+}
+
+void ContactManagersPluginInfo::clear()
+{
+  search_paths.clear();
+  search_libraries.clear();
+  discrete_plugin_infos.clear();
+  continuous_plugin_infos.clear();
+}
+
+bool ContactManagersPluginInfo::empty() const
+{
+  return (search_paths.empty() && search_libraries.empty() && discrete_plugin_infos.plugins.empty() &&
+          continuous_plugin_infos.plugins.empty());
+}
+
+bool ContactManagersPluginInfo::operator==(const ContactManagersPluginInfo& rhs) const
+{
+  bool equal = true;
+  equal &= isIdenticalSet<std::string>(search_paths, rhs.search_paths);
+  equal &= isIdenticalSet<std::string>(search_libraries, rhs.search_libraries);
+  equal &= discrete_plugin_infos == rhs.discrete_plugin_infos;
+  equal &= continuous_plugin_infos == rhs.continuous_plugin_infos;
+
+  return equal;
+}
+bool ContactManagersPluginInfo::operator!=(const ContactManagersPluginInfo& rhs) const { return !operator==(rhs); }
+
+template <class Archive>
+void ContactManagersPluginInfo::serialize(Archive& ar, const unsigned int /*version*/)
+{
+  ar& BOOST_SERIALIZATION_NVP(search_paths);
+  ar& BOOST_SERIALIZATION_NVP(search_libraries);
+  ar& BOOST_SERIALIZATION_NVP(discrete_plugin_infos);
+  ar& BOOST_SERIALIZATION_NVP(continuous_plugin_infos);
+}
+
+/*********************************************************/
+/******               CalibrationInfo                *****/
+/*********************************************************/
+void CalibrationInfo::insert(const CalibrationInfo& other) { joints.insert(other.joints.begin(), other.joints.end()); }
+
+void CalibrationInfo::clear() { joints.clear(); }
+
+bool CalibrationInfo::empty() const { return joints.empty(); }
+
+bool CalibrationInfo::operator==(const CalibrationInfo& rhs) const
+{
+  auto isometry_equal = [](const Eigen::Isometry3d& iso_1, const Eigen::Isometry3d& iso_2) {
+    return iso_1.isApprox(iso_2, 1e-5);
+  };
+
+  bool equal = true;
+  equal &= tesseract_common::isIdenticalMap<TransformMap, Eigen::Isometry3d>(joints, rhs.joints, isometry_equal);
+
+  return equal;
+}
+bool CalibrationInfo::operator!=(const CalibrationInfo& rhs) const { return !operator==(rhs); }
+
+template <class Archive>
+void CalibrationInfo::serialize(Archive& ar, const unsigned int /*version*/)
+{
+  ar& BOOST_SERIALIZATION_NVP(joints);
+}
+}  // namespace tesseract_common
+
+#include <tesseract_common/serialization.h>
+TESSERACT_SERIALIZE_ARCHIVES_INSTANTIATE(tesseract_common::PluginInfo)
+BOOST_CLASS_EXPORT_IMPLEMENT(tesseract_common::PluginInfo)
+TESSERACT_SERIALIZE_ARCHIVES_INSTANTIATE(tesseract_common::PluginInfoContainer)
+BOOST_CLASS_EXPORT_IMPLEMENT(tesseract_common::PluginInfoContainer)
+TESSERACT_SERIALIZE_ARCHIVES_INSTANTIATE(tesseract_common::KinematicsPluginInfo)
+BOOST_CLASS_EXPORT_IMPLEMENT(tesseract_common::KinematicsPluginInfo)
+TESSERACT_SERIALIZE_ARCHIVES_INSTANTIATE(tesseract_common::ContactManagersPluginInfo)
+BOOST_CLASS_EXPORT_IMPLEMENT(tesseract_common::ContactManagersPluginInfo)
+TESSERACT_SERIALIZE_ARCHIVES_INSTANTIATE(tesseract_common::CalibrationInfo)
+BOOST_CLASS_EXPORT_IMPLEMENT(tesseract_common::CalibrationInfo)
