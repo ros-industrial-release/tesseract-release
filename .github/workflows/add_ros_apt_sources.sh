769e639520cbb58f7ad11a2a6c2f7dcae7f1128b
Levi Armstrong <levi.armstrong@gmail.com>
2023-06-29 16:36:48 -0500

    Trimmed the branch to only the tesseract_collision sub directory

--- .github/workflows/add_ros_apt_sources.sh
+++
@@ -1,4 +0,0 @@
-#! /bin/bash
-sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
-curl -s https://raw.githubusercontent.com/ros/rosdistro/master/ros.asc | apt-key add -
-apt update -qq
